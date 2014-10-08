08c3c0d7bb0952f7b417eaf4c9feefe5ab79bf19
Philipp Otto <philipp@scm.io>
2014-10-08 17:29:38 +0200

    solve exercise 1

--- mlclass-ex1/gradientDescentMulti.m
+++ mlclass-ex1/gradientDescentMulti.m
@@ -11,25 +11,19 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
-
-
-
-
-
-
-
-
+    errorList = (X * theta - y);
+    bigSum = sum(repmat(errorList, 1, size(X)(2)) .* X);
+    theta = theta - (alpha / m * bigSum)';
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
