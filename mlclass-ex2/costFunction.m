79b600c47f05a906ed2740e9b9f0fb443cc82ee7
Philipp Otto <philipp@scm.io>
2014-10-28 15:07:08 +0100

    solve assignment 2

--- mlclass-ex2/costFunction.m
+++ mlclass-ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,11 +20,17 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+% theta: 3 zeilen, 1 spalte
+% X: 100 zeilen, 3 spalten
+% X * theta: 100 zeilen, 1 spalte
 
 
+hOfX = sigmoid(X * theta);
+J = 1 / m * sum(-y .* log(hOfX) - (1 - y) .* log(1 - hOfX));
 
 
-
+repeated = repmat(hOfX - y, 1, size(X)(2));
+grad = 1 / m * sum(repeated .* X);
 
 
 % =============================================================
--- mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/costFunctionReg.m
@@ -2,14 +2,16 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
+
+% theta : 28 x 1
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -17,9 +19,14 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+hOfX = sigmoid(X * theta);
+J = 1/m * sum(-y .* log(hOfX) - (1-y) .* log(1 - hOfX)) + lambda / (2*m) * sum(theta(2:end) .^ 2);
 
+repeated = repmat(hOfX - y, 1, size(X)(2));
+normalCost = 1 / m * sum(repeated .* X);
 
-
+grad       = normalCost + lambda/m * theta';
+grad(1, 1) = normalCost(1, 1);
 
 
 % =============================================================
